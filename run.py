3f4a56165308f4273481898075487d9975f9aae2
Sash <sasheng04@gmail.com>
2022-06-09 14:13:27 +0000

    Add name validation function

--- run.py
+++ run.py
@@ -13,7 +13,67 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('brew_that')
 
 sales = SHEET.worksheet('sales')
-
 data = sales.get_all_values()
 
-print(data)
+# Welcome Message
+
+def welcome_message():
+    """
+    Welcome the customer to BrewThat.
+    Service offering.
+    """
+    print("Welcome to BrewThat!\n")
+    print("A mobile pitstop that fuels cyclists on their socials.")
+
+# User Information
+def user_info():
+    """
+    Ask the user for their name to start order process.
+    """
+    print("Let's start brewing!\n")
+    print("First provide us with your name")
+
+
+def validate_username(name):
+    """
+    Check customers name for length, numbers and characters.
+    """
+    try:
+        if name.isnumeric():
+            raise ValueError(
+                "Enter your name with letters only please.")
+        
+        if not name.isalpha():
+            raise ValueError(
+                "Enter your name with letters only please.")
+
+        if len(name) < 2:
+            raise ValueError(
+                "Please ensure that your name is more than two letters long")
+    
+    except ValueError as e:
+        print(f"{e} \nPlease enter your name again")
+        return False
+    
+    return True
+
+# Formatting
+def end_section():
+    “”"
+    Print ### to end each section.
+    “”"
+    print(” “)
+    print(“# “* 25)
+    print(” “)
+
+
+# Main
+def main():
+    """
+    Run the main code functions
+    """
+    # Welcome Message
+    welcome_message()
+    end_section()
+    
+main() 
